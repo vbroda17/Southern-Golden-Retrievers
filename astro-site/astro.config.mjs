21460e82427e95bc3f1059264caa168dfa2551ce
Vincent Broda <vincentbroda@gmail.com>
2026-01-22 12:49:04 -0500

    new strucutre layout

---
+++ astro-site/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://vbroda17.github.io',
+    base: '/Knoxville-Golden-Retrivers',
+    outDir: '../docs',
+});
